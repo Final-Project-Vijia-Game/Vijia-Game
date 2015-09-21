83a1bcbcbe6cdbd73115cee44496d988fcde8ed9
Johnathan Chei <j.chei175@gmail.com>
2015-09-21 11:45:47 -0400

    added minimap, credit screen, and import new ghoul asset

---
+++ Game/Assets/MiniMapScript.js
@@ -0,0 +1,8 @@
+﻿//Target will be the player object
+var Target : GameObject;
+function Update () {
+
+	//Follows the player
+	gameObject.transform.position.x = Target.transform.position.x;
+	gameObject.transform.position.z = Target.transform.position.z;
+}
--- Game/Assets/Scripts/NewGameScript.js
+++ Game/Assets/Scripts/NewGameScript.js
@@ -1,6 +1,7 @@
 ﻿#pragma strict
 //functions to use for menu buttons
 
+//Despite naming convention this will also be used to change the scene to credit page.
 function NewGameSession (scene : int) {
 	//the scenes of the project exist in an array of sorts. scene will be an integer that will correspond to a scene
 	//and change to that scene
